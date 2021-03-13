70c11829d4a1ba50900a30b1351241b25235af1e
crepes-tyler-yu <yujikuang@126.com>
2021-03-14 03:12:13 +0800

    [Description] 完成代码构建

---
+++ src/main/java/pink/crepes/crepesframework/CrepesFrameworkApplication.java
@@ -0,0 +1,13 @@
+package pink.crepes.crepesframework;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrepesFrameworkApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CrepesFrameworkApplication.class, args);
+    }
+
+}
---
+++ src/test/java/pink/crepes/crepesframework/CrepesFrameworkApplicationTests.java
@@ -0,0 +1,13 @@
+package pink.crepes.crepesframework;
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrepesFrameworkApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
